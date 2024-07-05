d8dc5a48b2ceae1deec82db620d2b6ce5a5864b2
Erol <122645226+erolisldk@users.noreply.github.com>
2024-07-05 16:16:21 +0300

    1

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+CYPRESS_RECORD_KEY= "8e0f42e1-dc9e-4822-9dc1-b1ec8acfcdc0";
+
 
 module.exports = defineConfig({
   e2e: {
@@ -14,5 +16,6 @@ module.exports = defineConfig({
       return config;
     },
 	specPattern: "**/*.feature",
+ 
   },
 })
