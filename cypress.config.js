731c18988d9f5d4582a82ed6327e0b8223233b48
Erol <122645226+erolisldk@users.noreply.github.com>
2024-07-10 14:28:22 +0300

    7/10/2024 14:21

--- cypress.config.js
+++ cypress.config.js
@@ -6,25 +6,18 @@ CYPRESS_RECORD_KEY= "8e0f42e1-dc9e-4822-9dc1-b1ec8acfcdc0";
 
 
 module.exports = defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
-  reporterOptions: {
-    charts: true,
-    reportPageTitle: 'Optimum7.com Test Results',
-    embeddedScreenshots: true,
-    inlineAssets: true,
-    saveAllAttempts: false,
-  },
   e2e: {
     setupNodeEvents(on, config) {
-      on('before:run', async (details) => {
-        console.log('override before:run');
-        await beforeRunHook(details);
-      });
-
-      on('after:run', async () => {
-        console.log('override after:run');
-        await afterRunHook();
-      });
+      on("file:preprocessor",
+      createBundler({
+        plugins: [createEsbuildPlugin.default(config)],
+      }));
+      
+      preprocessor.addCucumberPreprocessorPlugin(on, config);
+      return config;
     },
+	specPattern: "**/*.feature",
+ 
   },
-});
+  //baseUrl: "https://www.optimum7.com",
+})
